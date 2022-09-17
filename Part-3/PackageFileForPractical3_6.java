924f85fee82696b3390a9574f168026f3cf3748b
21ce114 <97016150+21ce114@users.noreply.github.com>
2022-09-17 13:34:48 +0530

    Delete Part-3 directory

--- Part-3/PackageFileForPractical3_6.java
+++
@@ -1,8 +0,0 @@
-package part_3;
-
-public class PackageFileForPractical3_6 {
-	public void add (int a,int b) {
-		System.out.println("The sum is :"+(a+b));
-	}
-		
-}
--- Part-3/Practical3_5_import.java
+++
@@ -1,9 +0,0 @@
-import part_3.Practical3_5_Package_file;
-
-
-public class Practical3_5_import {
-    public static void main(String[] args) {
-    	Practical3_5_Package_file obj = new Practical3_5_Package_file();
-        obj.print();
-    }
-}
