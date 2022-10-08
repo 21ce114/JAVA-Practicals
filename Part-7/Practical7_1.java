27d47e2be4ed7a14e77ac32ff16b84c2daadb48d
21ce114 <97016150+21ce114@users.noreply.github.com>
2022-10-09 00:45:46 +0530

    Add files via upload

---
+++ Part-7/Practical7_1.java
@@ -0,0 +1,30 @@
+/*ID: 21CE114
+Name: Harsh Rana
+Git Repository Link: 
+https://github.com/21ce114/JAVA-Practicals.git
+AIM :Create a generic method for sorting an array of Comparable objects.
+*/
+
+import java.util.Arrays;
+
+class Sample{
+    public int num;
+    public Sample(int num){
+        this.num = num;
+    }
+    public String toString(){
+        return String.format("Number:"+ num);
+    }
+}
+
+public class Practical7_1{
+    public static void main(String[] args){
+        Sample[] samples = Arrays.asList(new Sample( 97),
+                                             new Sample( 114),
+                                             new Sample( 103)
+                                            ).toArray(new Sample[3]);
+
+        Arrays.sort(samples, (a, b) -> a.num - b.num);
+        Arrays.stream(samples).forEach(System.out :: println);
+    }
+}
